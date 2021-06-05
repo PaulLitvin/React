bb7d0489e1da7f24ddd90cd524f491a70c247dff
Pavlo_Lytvyn <pashalitvin10@gmail.com>
2021-06-05 17:25:32 +0300

    Updates

---
+++ precache-manifest.4214739da534e54f72129879361a898e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc03aaae17d5e8378b9f526de082da7",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2ac4da99e47f0bf6bce9",
+    "url": "/React/static/css/main.83809d89.chunk.css"
+  },
+  {
+    "revision": "4c8f185cbfd9f9b828d6",
+    "url": "/React/static/js/2.44bde625.chunk.js"
+  },
+  {
+    "revision": "2ac4da99e47f0bf6bce9",
+    "url": "/React/static/js/main.b7a11c65.chunk.js"
+  },
+  {
+    "revision": "4bdfff530aa6ef616196",
+    "url": "/React/static/js/runtime~main.1359234b.js"
+  }
+]);
